c123f6d83dc3b14f34b6f65e7a4e99d0bfbc5090
Burak Karaoglu <mkaraoglu@intelepeer.com>
2019-02-01 12:31:41 -0700

    Added ShapeCollection.java and README.md file

---
+++ HW1/ShapeCollection.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+abstract class Shape implements Comparable<Shape>{
+
+    protected String color;
+    protected int area;
+
+    public Shape(String color, int area){
+		    this.color=color;
+		    this.area=area;
+	  }
+
+	  public abstract void display();
+
+	  @Override
+	  public int compareTo(Shape s){
+        if(this.area==s.area)
+            return 0;
+        else if(this.area>s.area)
+            return 1;
+        else
+            return -1;
+    }
+
+    public void setColor(String color){
+	      this.color = color;
+	  }
+
+	  public String getColor(){
+	      return(this.color);
+	  }
+
+    public void setArea(int area){
+	      this.area = area;
+	  }
+
+    public int getArea(){
+	      return(this.area);
+	  }
+}
+
+class Triangle extends Shape {
+    public Triangle(String color, int area) {
+		    super(color, area);
+	  }
+
+	  @Override
+	  public void display(){
+	      System.out.println("I am a " + this.color + " triangle with area " + this.area);
+	  }
+}
+
+class Circle extends Shape {
+    public Circle(String color, int area) {
+		    super(color, area);
+	  }
+
+	  @Override
+	  public void display(){
+	      System.out.println("I am a " + this.color + " circle with area " + this.area);
+	  }
+}
+
+class Square extends Shape {
+    public Square(String color, int area) {
+		    super(color, area);
+	  }
+
+    @Override
+	  public void display(){
+	      System.out.println("I am a " + this.color + " square with area " +   this.area);
+	}
+}
+
+public class ShapeCollection{
+
+     public static void main(String []args){
+
+        ArrayList<Shape> shapes = new ArrayList<Shape>();
+        shapes.add(new Triangle("blue", 4));
+        shapes.add(new Square("green", 3));
+        shapes.add(new Circle("red", 5));
+
+        Collections.sort(shapes);
+
+        shapes.forEach(shape -> shape.display());
+}
